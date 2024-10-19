216b8465273b54b41b675ceac093606155667686
zhongwen-4 <2401128923@qq.com>
2024-10-19 21:40:42 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,65 @@
+<div align="center">
+  <a href="https://v2.nonebot.dev/store"><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/nbp_logo.png" width="180" height="180" alt="NoneBotPluginLogo"></a>
+  <br>
+  <p><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/NoneBotPlugin.svg" width="240" alt="NoneBotPluginText"></p>
+</div>
+
+<div align="center">
+
+# nonebot-plugin-picsetu
+
+_✨ 基于NoneBot的涩图插件 ✨_
+
+
+</div>
+
+## 📖 介绍
+
+一个涩图插件，就是你们想的那样
+
+## 💿 安装
+
+<details open>
+<summary>使用 nb-cli 安装</summary>
+在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装
+
+    nb plugin install nonebot-plugin-picsetu
+
+</details>
+
+<details>
+<summary>使用包管理器安装</summary>
+在 nonebot2 项目的插件目录下, 打开命令行, 根据你使用的包管理器, 输入相应的安装命令
+
+<details>
+<summary>pip</summary>
+
+    pip install nonebot-plugin-picsetu
+</details>
+<details>
+<summary>pdm</summary>
+
+    pdm add nonebot-plugin-picsetu
+</details>
+<details>
+<summary>poetry</summary>
+
+    poetry add nonebot-plugin-picsetu
+</details>
+<details>
+<summary>conda</summary>
+
+    conda install nonebot-plugin-picsetu
+</details>
+
+打开 nonebot2 项目根目录下的 `pyproject.toml` 文件, 在 `[tool.nonebot]` 部分追加写入
+
+    plugins = ["nonebot_plugin_picsetu"]
+
+</details>
+
+## 🎉 使用
+### 指令表
+| 指令 | 权限 | 需要@ | 范围 | 说明 |
+|:-----:|:----:|:----:|:----:|:----:|
+| setu | 所有人 | 否 | 通用 | 别忘了加前缀啊 |
---
+++ nonebot_plugin_picsetu/__init__.py
@@ -0,0 +1,62 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.plugin import PluginMetadata
+
+import httpx, random
+setu_cmd = on_command("setu", aliases={"色图", "涩图"})
+
+
+__plugin_meta__ = PluginMetadata(
+    name="涩图插件",
+    description="随机获取一张涩图",
+    type="application",
+    homepage="https://github.com/zhongwen-4/nonebot-plugin-picsetu",
+    supported_adapters={"~onebot.v11"}
+)
+
+
+@setu_cmd.handle()
+async def setu_handle():
+    a_randint = random.randint(1, 100)
+    if a_randint < 20:
+        await setu_cmd.send("稍等，图片正在赶来的路上")
+
+        async with httpx.AsyncClient() as client:
+            res = await client.get(
+                "https://api.lolicon.app/setu/v2"
+            )
+            url = await client.get(res.json()["data"][0]["urls"]["original"])
+
+            if url.status_code == 200:
+                await setu_cmd.finish(MessageSegment.image(res.json()["data"][0]["urls"]["original"]))
+            else:
+                await setu_cmd.finish("出错了，请重试")
+
+    
+    if a_randint < 70 and a_randint > 20:
+        await setu_cmd.send("稍等，图片正在赶来的路上")
+
+        async with httpx.AsyncClient() as client:
+            res = await client.get(
+                "https://image.anosu.top/pixiv/json"
+            )
+            url = await client.get(res.json()[0]["url"])
+
+            if url.status_code == 200:
+                await setu_cmd.finish(MessageSegment.image(res.json()[0]["url"]))
+            else:
+                await setu_cmd.finish("出错了，请重试")
+
+
+    if a_randint > 70:
+        await setu_cmd.send("稍等，图片正在赶来的路上")
+        async with httpx.AsyncClient() as client:
+            res = await client.get(
+                "https://misaliu.top/api/randomfurry/?format=json"
+            )
+
+            url = await client.get(res.json()[0]["url"])
+            if url.status_code == 200:
+                await setu_cmd.finish(MessageSegment.image(res.json()[0]["url"]))
+            else:
+                await setu_cmd.finish("出错了，请重试")
