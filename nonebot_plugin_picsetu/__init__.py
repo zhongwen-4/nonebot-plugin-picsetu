81cafb379a39308f9c96d1a8df974413456e9e92
zhongwen-4 <2401128923@qq.com>
2024-10-29 21:03:35 +0800

    修复某api获取图片链接失败的问题, closes #1

--- nonebot_plugin_picsetu/__init__.py
+++ nonebot_plugin_picsetu/__init__.py
@@ -10,7 +10,7 @@ __plugin_meta__ = PluginMetadata(
     name="涩图插件",
     description="随机获取一张涩图",
     type="application",
-    homepage="https://github.com/zhongwen-4/nonebot-plugin-picsetu",
+    homepage="https://github.com/zhongwen-4/nonebot-plugin-setu",
     supported_adapters={"~onebot.v11"},
     usage="发送[setu, 涩图, 色图]任意一个就可以获取图片啦"
 )
@@ -19,45 +19,50 @@ __plugin_meta__ = PluginMetadata(
 @setu_cmd.handle()
 async def setu_handle():
     a_randint = random.randint(1, 100)
-    if a_randint < 20:
-        await setu_cmd.send("稍等，图片正在赶来的路上")
 
-        async with httpx.AsyncClient() as client:
-            res = await client.get(
-                "https://api.lolicon.app/setu/v2"
-            )
-            url = await client.get(res.json()["data"][0]["urls"]["original"])
+    try:
+        if a_randint < 20:
+            await setu_cmd.send("稍等，图片正在赶来的路上")
 
-            if url.status_code == 200:
-                await setu_cmd.finish(MessageSegment.image(res.json()["data"][0]["urls"]["original"]))
-            else:
-                await setu_cmd.finish("出错了，请重试")
+            async with httpx.AsyncClient() as client:
+                res = await client.get(
+                    "https://api.lolicon.app/setu/v2"
+                )
+                url = await client.get(res.json()["data"][0]["urls"]["original"])
 
-    
-    if a_randint < 70 and a_randint > 20:
-        await setu_cmd.send("稍等，图片正在赶来的路上")
+                if url.status_code == 200:
+                    await setu_cmd.finish(MessageSegment.image(res.json()["data"][0]["urls"]["original"]))
+                else:
+                    await setu_cmd.finish("出错了，请重试")
 
-        async with httpx.AsyncClient() as client:
-            res = await client.get(
-                "https://image.anosu.top/pixiv/json"
-            )
-            url = await client.get(res.json()[0]["url"])
+        
+        if a_randint < 70 and a_randint > 20:
+            await setu_cmd.send("稍等，图片正在赶来的路上")
 
-            if url.status_code == 200:
-                await setu_cmd.finish(MessageSegment.image(res.json()[0]["url"]))
-            else:
-                await setu_cmd.finish("出错了，请重试")
+            async with httpx.AsyncClient() as client:
+                res = await client.get(
+                    "https://image.anosu.top/pixiv/json"
+                )
+                url = await client.get(res.json()[0]["url"])
+
+                if url.status_code == 200:
+                    await setu_cmd.finish(MessageSegment.image(res.json()[0]["url"]))
+                else:
+                    await setu_cmd.finish("出错了，请重试")
 
 
-    if a_randint > 70:
-        await setu_cmd.send("稍等，图片正在赶来的路上")
-        async with httpx.AsyncClient() as client:
-            res = await client.get(
-                "https://misaliu.top/api/randomfurry/?format=json"
-            )
+        if a_randint > 70:
+            await setu_cmd.send("稍等，图片正在赶来的路上")
+            async with httpx.AsyncClient() as client:
+                res = await client.get(
+                    "https://misaliu.top/api/randomfurry/?format=json"
+                )
 
-            url = await client.get(res.json()[0]["url"])
-            if url.status_code == 200:
-                await setu_cmd.finish(MessageSegment.image(res.json()[0]["url"]))
-            else:
-                await setu_cmd.finish("出错了，请重试")
+                url = await client.get(res.json()["url"])
+                if url.status_code == 200:
+                    await setu_cmd.finish(MessageSegment.image(res.json()["url"]))
+                else:
+                    await setu_cmd.finish("出错了，请重试")
+
+    except KeyError as e:
+        await setu_cmd.finish(f"图片链接获取失败，请重试，code: {e}")
